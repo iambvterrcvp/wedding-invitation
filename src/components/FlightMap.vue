e7f971c7e2461f219ec8795333766b3e0f03e7c2
Ceffa93 <ceffa93@gmail.com>
2025-09-06 13:31:04 +0900

    Complete walking loop with windows and floor/wall

--- src/components/FlightMap.vue
+++ src/components/FlightMap.vue
@@ -9,6 +9,9 @@ class Vec2 {
   }
 }
 
+const mod = (n, m) => ((n % m) + m) % m
+
+
 const keys = {};
 window.addEventListener("keydown", (e) => { keys[e.key] = true; });
 window.addEventListener("keyup", (e) => { keys[e.key] = false; });
@@ -21,8 +24,8 @@ const timestep = 1000 / 60
 let lastTime = performance.now()
 let accumulator = 0
 
-let planePos = reactive(new Vec2(brideSize.x/2, canvasSize.y - brideSize.y/2))
-let planeScale = reactive(new Vec2(1,1))
+let bridePos = reactive(new Vec2(brideSize.x/2, canvasSize.y - brideSize.y/2 - 5))
+let brideScale = reactive(new Vec2(1,1))
 
 const update = (delta) => {
   let speed = .02
@@ -30,9 +33,9 @@ const update = (delta) => {
   if (keys["ArrowLeft"]) dir.x = -1; else if (keys["ArrowRight"]) dir.x = 1
   
   if (dir.x != 0) {
-    planeScale.x = dir.x > 0 ? 1 : -1
+    brideScale.x = dir.x > 0 ? 1 : -1
   } 
-  planePos.x = planePos.x + dir.x * timestep * speed
+  bridePos.x = bridePos.x + dir.x * timestep * speed
 }
 
 function loop() {
@@ -62,9 +65,86 @@ onMounted(() => {
       class="relative h-fit w-full max-w-4xl overflow-hidden border-4 border-black bg-blue-200 sm:h-96"
     >
       <svg class="h-full w-full" :viewBox="`0 0 ${canvasSize.x} ${canvasSize.y}`" preserveAspectRatio="xMidYMid meet">
+        <pattern
+          id="wallPattern"
+          patternUnits="userSpaceOnUse"
+          :x="-bridePos.x"
+          :width="5"
+          :height="5">
+          <image
+          href="/wall_dark2.jpg"
+          :x="0"
+          :y="0"
+          :width="5"
+          :height="5"
+          />
+        </pattern>
+        <pattern
+          id="floorPattern"
+          patternUnits="userSpaceOnUse"
+          :x="-bridePos.x"
+          :width="5"
+          :height="5">
+          <image
+          href="/dark_wood.jpg"
+          :x="0"
+          :y="0"
+          :width="5"
+          :height="5"
+          />
+        </pattern>
+    <rect
+      x="0"
+      y="0"
+      :width="canvasSize.x"
+      :height="canvasSize.y * 3/4"
+      fill="url(#wallPattern)"
+    />
+    <rect
+      :x="0"
+      :y="canvasSize.y * 3/4"
+      :width="canvasSize.x"
+      :height="canvasSize.y * 1/4"
+      fill="url(#floorPattern)"
+    />
+        <image
+          href="/window.png"
+          :x="-canvasSize.x*1/3 + mod(canvasSize.x * 1/3 -bridePos.x, canvasSize.x*5/3)"
+          :y="canvasSize.y /2 - 16 - 7"
+          :width="32"
+          :height="32"
+        />
+        <image
+          href="/window.png"
+          :x="-canvasSize.x*1/3 + mod(canvasSize.x * 2/3 -bridePos.x, canvasSize.x*5/3)"
+          :y="canvasSize.y /2 - 16 - 7"
+          :width="32"
+          :height="32"
+        />
+        <image
+          href="/window.png"
+          :x="-canvasSize.x*1/3 + mod(canvasSize.x * 3/3 -bridePos.x, canvasSize.x*5/3)"
+          :y="canvasSize.y /2 - 16 - 7"
+          :width="32"
+          :height="32"
+        />
+        <image
+          href="/window.png"
+          :x="-canvasSize.x*1/3 + mod(canvasSize.x * 4/3 -bridePos.x, canvasSize.x*5/3)"
+          :y="canvasSize.y /2 - 16 - 7"
+          :width="32"
+          :height="32"
+        />
+        <image
+          href="/window.png"
+          :x="-canvasSize.x*1/3 + mod(canvasSize.x * 5/3 -bridePos.x, canvasSize.x*5/3)"
+          :y="canvasSize.y /2 - 16 - 7"
+          :width="32"
+          :height="32"
+        />
 
         <g
-          :transform="`translate(${planePos.x}, ${planePos.y}) scale(${planeScale.x}, ${planeScale.y})`"
+          :transform="`translate(10, ${bridePos.y})`"
           class="none"
         >
           <image
