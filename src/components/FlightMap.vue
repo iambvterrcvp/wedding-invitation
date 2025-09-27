15aa0fdbdae3c7c252b3a07f497cc572a1efaa98
Ceffa93 <ceffa93@gmail.com>
2025-09-27 17:38:10 +0900

    Added rifle

--- src/components/FlightMap.vue
+++ src/components/FlightMap.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { ref, onMounted, computed, reactive } from 'vue'
+import { ref, onMounted, reactive } from 'vue'
 const props = defineProps()
 
 class Vec2 {
@@ -8,6 +8,20 @@ class Vec2 {
     this.y = y;
   }
 }
+
+let mouse = reactive(new Vec2(0, 0))
+const canvasRef = ref(null);
+
+function updateMouse(e) {
+  const svg = canvasRef.value;
+  const pt = svg.createSVGPoint();
+  pt.x = e.clientX;
+  pt.y = e.clientY;
+  const svgP = pt.matrixTransform(svg.getScreenCTM().inverse());
+  mouse.x = svgP.x;
+  mouse.y = svgP.y;
+}
+
 
 
 
@@ -20,13 +34,18 @@ window.addEventListener("keyup", (e) => { keys[e.key] = false; });
 
 let canvasSize = new Vec2(100, 60)
 let brideSize = new Vec2(16, 16)
+const bridePos = new Vec2(2, canvasSize.y - brideSize.y - 5)
+const rifleSize = new Vec2(12, 12)
+const riflePos = new Vec2(bridePos.x + 5, bridePos.y + 3.5)
+const rifleRotPivot = new Vec2(riflePos.x + 3, riflePos.y + 5)
+const rifleRange = new Vec2(30, -40)
 
 let frameIdx = 0
 const timestep = 1000 / 60
 let lastTime = performance.now()
 let accumulator = 0
 
-let bridePos = reactive(new Vec2(brideSize.x/2, canvasSize.y - brideSize.y/2 - 5))
+let walkedDistance = reactive(new Vec2(0,0))
 let brideScale = reactive(new Vec2(1,1))
 
 let windows = []
@@ -38,13 +57,14 @@ update_windows()
 
 function update_windows() {
   for (let i = 0; i < windowsCount; i++) {
-    windows[i].x = -canvasSize.x*1/3 + mod(canvasSize.x * (i+1)/3 -bridePos.x, canvasSize.x*5/3)
+    windows[i].x = -canvasSize.x*1/3 + mod(canvasSize.x * (i+1)/3 -walkedDistance.x, canvasSize.x*5/3)
     windows[i].y = canvasSize.y /2 - 16 - 7
   }
 }
 
-const update = (delta) => {
+let rifleAngle = ref(0)
 
+const update = (delta) => {
   let speed = .02
   let dir = new Vec2(0,0)
   if (keys["ArrowLeft"]) dir.x = -1; else if (keys["ArrowRight"]) dir.x = 1
@@ -52,8 +72,12 @@ const update = (delta) => {
   if (dir.x != 0) {
     brideScale.x = dir.x > 0 ? 1 : -1
   } 
-  bridePos.x = bridePos.x + dir.x * delta * speed
+  walkedDistance.x = walkedDistance.x + dir.x * delta * speed
 
+  let diffMouseBride = new Vec2(mouse.x - 2, mouse.y - (canvasSize.y - brideSize.y - 5))
+  let angle = Math.atan2(diffMouseBride.y, diffMouseBride.x) * 180 / Math.PI
+  angle = Math.min(Math.max(angle, rifleRange.y), rifleRange.x)
+  rifleAngle.value = angle
   update_windows()
 }
 
@@ -75,6 +99,7 @@ function loop() {
 
 onMounted(() => {
   requestAnimationFrame(loop);
+  window.addEventListener("mousemove", updateMouse);
 })
 </script>
 
@@ -83,11 +108,11 @@ onMounted(() => {
     <div
       class="relative h-fit w-full max-w-4xl overflow-hidden border-4 border-black bg-blue-200 sm:h-96"
     >
-      <svg class="h-full w-full" :viewBox="`0 0 ${canvasSize.x} ${canvasSize.y}`" preserveAspectRatio="xMidYMid meet">
+      <svg ref="canvasRef" class="h-full w-full" :viewBox="`0 0 ${canvasSize.x} ${canvasSize.y}`" preserveAspectRatio="xMidYMid meet">
         <pattern
           id="wallPattern"
           patternUnits="userSpaceOnUse"
-          :x="-bridePos.x"
+          :x="-walkedDistance.x"
           :width="5"
           :height="5">
           <image
@@ -101,7 +126,7 @@ onMounted(() => {
         <pattern
           id="floorPattern"
           patternUnits="userSpaceOnUse"
-          :x="-bridePos.x"
+          :x="-walkedDistance.x"
           :width="5"
           :height="5">
           <image
@@ -136,18 +161,21 @@ onMounted(() => {
           :height="32"
         />
       
-        <g
-          :transform="`translate(10, ${bridePos.y})`"
-          class="none"
-        >
-          <image
-            href="/bride2.png"
-            :x="-brideSize.x/2"
-            :y="-brideSize.y/2"
-            :width="brideSize.x"
-            :height="brideSize.y"
-          />
-        </g>
+        <image
+          href="/bride2.png"
+          :x="bridePos.x"
+          :y="bridePos.y"
+          :width="brideSize.x"
+          :height="brideSize.y"
+        />
+        <image
+          href="/rifle.png"
+          :x="riflePos.x"
+          :y="riflePos.y"
+          :width="rifleSize.x"
+          :height="rifleSize.y"
+          :transform="`rotate(${rifleAngle} ${rifleRotPivot.x} ${rifleRotPivot.y})` "
+        />
       </svg>
     </div>
   </div>
