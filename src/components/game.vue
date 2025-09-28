59ebf4663bfaf6fa8109d4bb5b4b207724ffd7e2
Ceffa93 <ceffa93@gmail.com>
2025-09-28 19:07:30 +0900

    Bullet collision, explosion, blood spatter implemented

--- src/components/game.vue
+++ src/components/game.vue
@@ -8,6 +8,14 @@ class Vec2 {
     this.y = y;
   }
 }
+class Explosion {
+  life = 0
+  pos = new Vec2(0, 0);
+}
+class Blood {
+  life = 0
+  pos = new Vec2(0, 0);
+}
 class Bullet {
   life = 0
   pos = new Vec2(0, 0);
@@ -17,6 +25,10 @@ class Nun {
   life = 0
   pos = new Vec2(0, 0);
   index = 0
+}
+class Priest {
+  life = 0
+  pos = new Vec2(0, 0);
 }
 
 let mouse = reactive(new Vec2(0, 0))
@@ -45,12 +57,14 @@ window.addEventListener("keyup", (e) => { keys[e.key] = false; });
 let canvasSize = new Vec2(100, 60)
 const brideSize = new Vec2(16, 16)
 const nunSize = new Vec2(22, 22)
+const priestSize = new Vec2(18, 18)
 const bridePos = new Vec2(2, canvasSize.y - brideSize.y - 5)
 const rifleSize = new Vec2(12, 12)
 const riflePos = new Vec2(bridePos.x + 3, bridePos.y + 2.5)
 const rifleRotPivot = new Vec2(riflePos.x + 5, riflePos.y + 5)
 const rifleRange = new Vec2(30, -40)
 const bulletSize = new Vec2(12, 12)
+const explosionSize = new Vec2(30,30)
 
 
 let frameIdx = 0
@@ -85,6 +99,11 @@ function update_bullets(delta) {
 }
 
 let nuns = ref([])
+function spawn_nun() {
+  let nun = new Nun()
+    nun.pos = new Vec2(bridePos.x + 100, 0)
+    nuns.value.push(nun)
+}
 function update_nuns(delta) {
   nuns.value.forEach(nun => {
     nun.life += delta
@@ -94,6 +113,71 @@ function update_nuns(delta) {
   });
   nuns.value = nuns.value.filter(nun => nun.life < 6000);
 }
+
+let explosions = ref([])
+function spawn_explosion(pos) {
+  let explosion = new Explosion()
+  explosion.pos = new Vec2(pos.x, pos.y - 5)
+  explosions.value.push(explosion)
+}
+function update_explosions(delta) {
+  explosions.value.forEach(explosion => {
+    explosion.life += delta
+    explosion.pos.x -= .02 * delta
+  });
+  explosions.value = explosions.value.filter(explosion => explosion.life < 1000);
+}
+
+let bloods = ref([])
+function spawn_blood(pos) {
+  let blood = new Blood()
+  blood.pos = new Vec2(pos.x, pos.y)
+  bloods.value.push(blood)
+}
+function update_bloods(delta) {
+  bloods.value.forEach(blood => {
+    blood.life += delta
+    blood.pos.x -= .02 * delta
+  });
+  bloods.value = bloods.value.filter(blood => blood.life < 6000);
+}
+
+let priests = ref([])
+function spawn_priest() {
+  let priest = new Priest()
+    priest.pos = new Vec2(bridePos.x + 100, bridePos.y - 2)
+    priests.value.push(priest)
+}
+function update_priests(delta) {
+  priests.value.forEach(priest => {
+    priest.life += delta
+    priest.pos.x -= .04 * delta;
+  });
+  priests.value = priests.value.filter(priest => priest.life < 6000);
+}
+
+function check_collision(a, b) {
+  let diff = new Vec2(a.pos.x - b.pos.x, a.pos.y - b.pos.y)
+  if (Math.abs(diff.x) < 5 && Math.abs(diff.y) < 10) {
+    b.life = 100000
+    a.life = 100000
+    return true
+  }
+  return false
+}
+
+function resolve_collisions() {
+  bullets.value.forEach(bullet => {
+    priests.value.forEach(priest => {
+      if (check_collision(bullet, priest)) spawn_explosion(priest.pos)
+    })
+    nuns.value.forEach(nun => {
+      if (check_collision(bullet, nun)) spawn_blood(nun.pos)
+    })
+  })
+  priests.value = priests.value.filter(priest => priest.life < 6000);
+}
+
 
 
 let rifleDir = reactive(new Vec2(0,0))
@@ -111,6 +195,9 @@ function dir_to_angle(dir) {
 
 let bulletDelay = 1000000
 let nunDelay = 1000000
+let priestDelay = 1000000
+
+let brideLife = ref(0)
 
 const update = (delta) => {
   const walkSpeed = .02
@@ -127,12 +214,16 @@ const update = (delta) => {
   nunDelay += delta
   if (nunDelay > 1000) {
     nunDelay = 0
-    let nun = new Nun()
-    nun.pos = new Vec2(bridePos.x + 100, 0)
-    nuns.value.push(nun)
+    spawn_nun()
   }
 
-  
+  priestDelay += delta
+  if (priestDelay > 1300) {
+    priestDelay = 0
+    spawn_priest()
+  }
+
+  brideLife.value += delta
   walkedDistance.x = walkedDistance.x + delta * walkSpeed
 
   rifleDir = normalize(new Vec2(mouse.x - 2, mouse.y - (canvasSize.y - brideSize.y - 5)))
@@ -140,7 +231,11 @@ const update = (delta) => {
   angle = Math.min(Math.max(angle, rifleRange.y), rifleRange.x)
   rifleAngle.value = angle
   update_windows()
+  resolve_collisions()
+  update_explosions(delta)
+  update_bloods(delta)
   update_nuns(delta)
+  update_priests(delta)
   update_bullets(delta)
 }
 
@@ -225,14 +320,24 @@ onMounted(() => {
           :width="32"
           :height="32"
         />
-      
         <image
-          href="/bride2.png"
-          :x="bridePos.x"
-          :y="bridePos.y"
-          :width="brideSize.x"
-          :height="brideSize.y"
-        />
+            v-for="(blood, i) in bloods"
+            :key="i"
+            href="/blood.png"
+            :x="blood.pos.x"
+            :y="blood.pos.y"
+            :width="24"
+            :height="24"
+          />
+          <image
+            v-for="(explosion, i) in explosions"
+            :key="i"
+            href="/explosion.png"
+            :width="explosionSize.x"
+            :height="explosionSize.y"
+            :opacity="1-Math.max(0,Math.min(1, (explosion.life-200) / 100))"
+            :transform="`translate(${explosion.pos.x + explosionSize.x/2}, ${explosion.pos.y + explosionSize.y/2}) scale(${Math.min(1, explosion.life / 100)}) translate(${-explosionSize.x/2}, ${-explosionSize.y/2})`"
+          />
         <image
           v-for="(nun, i) in nuns"
           :key="i"
@@ -243,6 +348,16 @@ onMounted(() => {
           :height="nunSize.y"
         />
         <image
+          v-for="(priest, i) in priests"
+          :key="i"
+          href="/priest.png"
+          :x="priest.pos.x"
+          :y="priest.pos.y"
+          :width="priestSize.x"
+          :height="priestSize.y"
+          :transform="`rotate(${priest.life % 200 > 100 ? 2 : -2} ${priest.pos.x + priestSize.x * 2/3} ${priest.pos.y + priestSize.y})` "
+        />
+        <image
         v-for="(bullet, i) in bullets"
           :key="i"
           href="/bouquet.png"
@@ -251,6 +366,14 @@ onMounted(() => {
           :width="bulletSize.x"
           :height="bulletSize.y"
           :transform="`rotate(${dir_to_angle(bullet.dir) + 90} ${bullet.pos.x + bulletSize.x/2} ${bullet.pos.y + bulletSize.y / 2})` "
+          />
+          <image
+            href="/bride2.png"
+            :x="bridePos.x"
+            :y="bridePos.y"
+            :width="brideSize.x"
+            :height="brideSize.y"
+            :transform="`rotate(${brideLife % 300 > 150 ? .5 : -.5} ${bridePos.x + brideSize.x /2} ${bridePos.y + brideSize.y})` "
           />
           <image
             href="/bazooka.png"
